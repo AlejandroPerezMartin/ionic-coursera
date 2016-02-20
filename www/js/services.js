1a2c239984bcf37c9b35675f6b451ed434c8edb4
Alejandro Pérez Martín <aleperez92@gmail.com>
2016-02-20 23:45:38 +0000

    Added localStorage service for login

--- www/js/services.js
+++ www/js/services.js
@@ -57,4 +57,21 @@ angular.module('conFusion.services', ['ngResource'])
 
 }])
 
+.factory('$localStorage', ['$window', function ($window) {
+  return {
+    store: function (key, value) {
+      $window.localStorage[key] = value;
+    },
+    get: function (key, defaultValue) {
+      return $window.localStorage[key] || defaultValue;
+    },
+    storeObject: function (key, value) {
+      $window.localStorage[key] = JSON.stringify(value);
+    },
+    getObject: function (key, defaultValue) {
+      return JSON.parse($window.localStorage[key] || defaultValue);
+    }
+  };
+}])
+
 ;
